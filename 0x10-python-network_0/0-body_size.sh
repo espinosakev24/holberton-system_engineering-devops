3fb2dac656f0c255d6b6cff06396d0282dd3677f
espinosakev24 <916@holbertonschool.com>
2020-01-06 15:35:01 -0500

    deleting 0x10 python network

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Request to a URL and printing only de content size in bytes
-curl -sI $1 | sed -n "/Content-Length/p" | cut -d " " -f 2
--- 0x10-python-network_0/README.md
+++
@@ -1,1 +0,0 @@
-Project: 0x10. Python - Network #0
