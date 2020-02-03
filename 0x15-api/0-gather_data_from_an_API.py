27aab804e83b151b7da19972e7a38d9d6559da96
espinosakev24 <916@holbertonschool.com>
2020-02-03 16:03:18 -0500

    fixes pep8 and typos

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,13 +12,13 @@ if __name__ == '__main__':
     user_id = requests.get(URL_TODO).json().get('id')
     user = requests.get(URL_USERS + str(user_id)).json()
     employee_name = user['name']
-    
     for task in todos:
         if task['userId'] == user_id:
             total += 1
             if task['completed']:
                 tasks_done.append(task['title'])
                 done += 1
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, done, total))
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, done, total))
     for task in tasks_done:
-        print("\t{}".format(task))
+        print("\t {}".format(task))
