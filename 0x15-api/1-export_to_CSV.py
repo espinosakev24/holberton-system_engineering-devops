0ef5b59c54abb60b89827a6f9001e098df4e1dfe
espinosakev24 <916@holbertonschool.com>
2020-02-03 17:03:12 -0500

    fixes username to be displayed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-""" exports to csv"""
-
-import csv
 import requests
 from sys import argv
+import csv
 
 if __name__ == '__main__':
     URL = 'https://jsonplaceholder.typicode.com/'
     URL_USERS = URL + 'users/'
+    records, list_records = [], []
     user_id = requests.get(URL_USERS + argv[1]).json()['id']
     todos = requests.get(URL + 'todos').json()
     user = requests.get(URL_USERS + str(user_id)).json()
     filename = str(user_id) + '.csv'
-    with open(filename, 'w') as fd:
-        wr = csv.writer(fd, delimiter=',', quoting=csv.QUOTE_ALL)
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todos:
             if task['userId'] == user.get('id'):
-                wr.writerow([user.get('id'), user['name'],
+                writer.writerow([user.get('id'), user['username'],
                                  task['completed'], task['title']])
