6448ee59808fe3da2a45f0d041c24ad7a605f50d
espinosakev24 <916@holbertonschool.com>
2020-02-03 18:29:22 -0500

    uploades task 3 -> dict of list of dicts

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+import json
+
+if __name__ == '__main__':
+    URL = 'https://jsonplaceholder.typicode.com/'
+    idx = 1
+    dict_file, records = {}, []
+    user_dict = {}
+    URL_TODO = URL + 'todos/'
+    URL_USERS = URL + 'users/'
+    todos = requests.get(URL_TODO).json()
+    users = requests.get(URL_USERS)
+
+    while idx <= 10:
+        task_user = requests.get(URL_TODO + '?userId=' + str(idx)).json()
+        userResponse = requests.get(URL_USERS + str(idx)).json()
+        for task in task_user:
+            records.append({"task": task['title'], "completed":
+                            task['completed'],
+                            "username": userResponse['username']})
+        user_dict.update({str(idx): records})
+        idx += 1
+
+    filename = "todo_all_employees.json"
+    with open(filename, 'w', encoding="utf-8") as jsonfile:
+        jsonfile.write(json.dumps(user_dict))
