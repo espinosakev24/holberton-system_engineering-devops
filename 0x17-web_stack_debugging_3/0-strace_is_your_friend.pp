633efd47cb9568e2e42c240fca1f9c268fb318c3
espinosakev24 <916@holbertonschool.com>
2020-02-19 11:40:06 -0500

    Fixes wpconf file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Changes phpp by php in wpconf
+exec { 'Fixes php conf':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php; sudo service apache2 restart',
+  provider    => 'shell',
+}
