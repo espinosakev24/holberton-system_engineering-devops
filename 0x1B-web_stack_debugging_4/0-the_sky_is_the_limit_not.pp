6748c9de79e6cfaeac50b14ab9c312e2ae0fa553
espinosakev24 <916@holbertonschool.com>
2020-02-27 00:28:51 -0500

    fixes amount of nginx processes allowed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Increase the limit of nginx processes
+exec { 'enlarge_processes':
+    command  => 'sed -i s/worker_processes 4;/worker_processes 100;/g /etc/nginx/nginx.conf; sudo service nginx restart',
+    provider => 'shell',
+}
